182f78ab2a8c0db332dfbb9fbb05bc81b8c4e260
耿志卫 <durian@gengzhiweideMacBook-Pro.local>
2018-09-29 23:08:29 +0800

    第十七章 容器深入研究

---
+++ src/main/java/com/jaaaelu/gzw/learn/java/thinkingInJava/containerInDepthStudy/Test.java
@@ -0,0 +1,11 @@
+package com.jaaaelu.gzw.learn.java.thinkingInJava.containerInDepthStudy;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(new Test().hashCode());
+        System.out.println(new Test().hashCode());
+        System.out.println(new Test().toString());
+        System.out.println(new Test().toString());
+    }
+}
